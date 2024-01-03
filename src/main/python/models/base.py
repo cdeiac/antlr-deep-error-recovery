55aed8df7690bcb64b60a3302d6c5bd8f018501e
Cristian De Iaco <cristian.deiaco@uzh.ch>
2024-01-03 16:37:22 +0100

    second round of model feedback

--- src/main/python/models/base.py
+++ src/main/python/models/base.py
@@ -48,35 +48,38 @@ class Encoder(nn.Module):
         else:
             h_s_last = h_s_last.squeeze(0)
         fc2_out = self.fc2(h_s_last)
-        fc2_out = nn.functional.relu(fc2_out)
-        enc_hidden = torch.cat([fc2_out] * self.num_layers)
-        return enc_hidden
+        #fc2_out = nn.functional.relu(fc2_out)
+        #enc_hidden = torch.cat([fc2_out] * self.num_layers)
+        #return enc_hidden
+        return fc2_out # [1, latent_size]
 
 
 class Decoder(nn.Module):
-    def __init__(self, input_size, hidden_size, num_layers, vocab_size, device, bidirectional=False):
+    def __init__(self, input_size, latent_size, hidden_size, num_layers, vocab_size, device, bidirectional=False):
         """
         Initializes the Decoder module.
 
         Args:
             input_size (int): The input of the decoder which is equal to batch_size since we feed in one token at a time
+            latent_size (int): The size of the context vector produced by the encoder
             hidden_size (int): The size of the hidden state in the LSTM, must match the size of the latent space of the encoder.
             num_layers (int): The number of layers in the LSTM.
             vocab_size (int): The size of the vocabulary which is the output size of the decoder.
-            device (str): The device (e.g., 'cuda' or 'cpu') on which the model will be run.
+            device (torch.device): The device (e.g., 'cuda' or 'cpu') on which the model will be run.
             bidirectional (bool, optional): Whether the LSTM is bidirectional. Defaults to False.
         """
         super(Decoder, self).__init__()
         self.device = device
         self.input_size = input_size
+        self.latent_size = latent_size
         self.num_layers = num_layers
         self.vocab_size = vocab_size
         self.bidirectional = bidirectional
         self.hidden_size = hidden_size
-        self.fc_size = self.hidden_size * 2 if self.bidirectional else self.hidden_size
 
+        self.fc_upscale = nn.Linear(self.latent_size, self.hidden_size)
         self.LSTM = nn.LSTM(self.input_size, self.hidden_size, self.num_layers, bidirectional=self.bidirectional)
-        self.fc = nn.Linear(self.fc_size, self.vocab_size)
+        self.fc_out = nn.Linear(self.hidden_size * 2 if self.bidirectional else self.hidden_size, self.vocab_size)
 
     def forward(self, y, context_vector, tfr=0.0):
         y_len = y.shape[0]
@@ -86,8 +89,12 @@ class Decoder(nn.Module):
         outputs = torch.zeros(y_len, self.vocab_size, dtype=torch.float32).to(self.device)
         # start with <SOS> token
         current_token = y[0]
+        # upscale context vector from latent_size to hidden_size
+        context_vector = self.fc_upscale(context_vector) # shape: [1, latent_size] -> [1, hidden_size]
+        context_vector = torch.cat([context_vector] * self.num_layers) # shape: [num_layers, hidden_size]
         h_s = context_vector
         h_c = context_vector
+        # TODO: Loop until a max size and add EOS termination condition when the model performs better!
         for i in range(1, y_len):
             predictions, (h_s, h_c) = self.__forward_iteration(current_token, h_s, h_c)
             outputs[i] = predictions
@@ -99,15 +106,15 @@ class Decoder(nn.Module):
     def __forward_iteration(self, x, h_s_in, h_c_in):
         x = x.view(-1, 1).to(torch.float32)
         output, (h_s, h_c) = self.LSTM(x, (h_s_in, h_c_in))
-        predictions = self.fc(output)
+        predictions = self.fc_out(output)
         predictions = nn.functional.relu(predictions)
         predictions = predictions.squeeze(0)
         return predictions, (h_s, h_c)
 
 
 class DenoisingAE(nn.Module):
-    def __init__(self, vocab_size, embedding_size, fc1_size, hidden_size, latent_size, num_layers, bidirectional, device,
-                 batch_size=1):
+    def __init__(self, vocab_size, embedding_size, fc1_size, hidden_size, latent_size,
+                 num_layers, bidirectional, device, batch_size=1):
         """
         Initializes the custom class.
 
@@ -119,12 +126,12 @@ class DenoisingAE(nn.Module):
             latent_size (int): The size of the latent space.
             num_layers (int): The number of LSTM layers for both the encoder and decoder
             bidirectional (bool): Whether the model is bidirectional.
-            device (str): The device (e.g., 'cuda' or 'cpu') on which the model will be run.
+            device (torch.device): The device (e.g., 'cuda' or 'cpu') on which the model will be run.
             batch_size (int, optional): The batch size. Defaults to 1.
         """
         super(DenoisingAE, self).__init__()
         self.encoder = Encoder(vocab_size, embedding_size, fc1_size, hidden_size, num_layers, latent_size, bidirectional)
-        self.decoder = Decoder(batch_size, latent_size, num_layers, vocab_size, device, bidirectional)
+        self.decoder = Decoder(batch_size, latent_size, hidden_size, num_layers, vocab_size, device, bidirectional)
 
     def forward(self, x, y, tfr=0.0):
         context_vector = self.encoder(x)
