856d62a36e55740e3bc9dc7f08e7aef8d7b87271
Cristian De Iaco <cristian.deiaco@uzh.ch>
2024-01-24 14:58:54 +0100

    extended scripts

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -2,7 +2,15 @@
 
 initial_directory=$(pwd)
 
-# installation
+# java 17 installation
+if type -p java && [[ $(java -version 2>&1 | awk '/version/ {print $3}' | cut -d'"' -f2) == "17" ]];
+then
+    echo "Java 17 is already installed."
+else
+    sudo apt install openjdk-17-jdk -y
+fi
+
+# maven installation
 if command -v mvn &> /dev/null
 then
   echo "Maven is already installed."
@@ -10,9 +18,18 @@ else
   sudo apt install -y maven
 fi
 
-# maven
-mvn clean package
 # python
+if command -v python3.8 &>/dev/null;
+then
+    echo "Python 3.8 is already installed."
+else
+    sudo apt apt install python3.8 python3.8-venv python3-venv -y
+fi
+
+# maven build
+mvn clean package
+
+# python venv
 cd src/main/python
 python3 -m venv venv
 source venv/bin/activate
