652d0b0328af396d7e181465edd2ab33893c815e
Cristian De Iaco <cristian.deiaco@uzh.ch>
2024-01-24 14:09:49 +0100

    create data directories in prepare script

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -18,5 +18,16 @@ python3 -m venv venv
 source venv/bin/activate
 pip install -r requirements.txt
 
-
 cd "$initial_directory"
+
+# create data directories
+mkdir src/main/resources/data src/main/resources/generated
+
+mkdir src/main/python/data/ src/main/python/data/generated
+mkdir src/main/python/data/generated/cache
+mkdir src/main/python/data/generated/checkpoints
+mkdir src/main/python/data/generated/cv
+mkdir src/main/python/logs
+
+
+
